b6b0a9b552d436743cac399b3a6ce63204712ce0
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-05-20 11:44:54 +0100

    add fill murray row

--- App/Views/Interests.js
+++ App/Views/Interests.js
@@ -10,15 +10,19 @@ import {
 
 const Row = () => {
   return (
-    <View style={styles.row}>
-      <Image
-        style={styles.tile}
-        source={{ uri: 'http://www.fillmurray.com/150/150' }}
-      />
-      <Image
-        style={styles.tile}
-        source={{ uri: 'http://www.fillmurray.com/150/150' }}
-      />
+    <View>
+      <View style={styles.row}>
+        <Image
+          style={styles.tile}
+          source={{ uri: 'http://www.fillmurray.com/150/150' }}
+          />
+        <View style={styles.divider} />
+        <Image
+          style={styles.tile}
+          source={{ uri: 'http://www.fillmurray.com/150/150' }}
+          />
+      </View>
+      <View style={styles.horizontalDiv} />
     </View>
   )
 }
@@ -51,9 +55,17 @@ const styles = StyleSheet.create({
   tile: {
     height: 150,
     width: 150,
-    flex: 1
+    flex: 14
   },
   row: {
     flexDirection: 'row'
+  },
+  divider: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  horizontalDiv: {
+    height: 2,
+    backgroundColor: 'white'
   }
 })
