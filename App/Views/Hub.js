0b8cd68002b1af4ee892a575392a33e1cf16bca6
Andrew <a.macmurray@icloud.com>
2016-05-21 10:47:20 +0100

    draft out tile component for events view

---
+++ App/Views/Hub.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native'
+
+import Tile from '../Components/Tile.js'
+
+export default class Hub extends Component {
+  navTester () {
+    this.props.navigator.push({
+      name: 'Event Info'
+    })
+  }
+
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>Events Near You</Text>
+        <ScrollView>
+          <Tile />
+
+          <TouchableHighlight style={styles.container} onPress={this.navTester.bind(this)}>
+            <Text>This is a tile</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.container} onPress={this.navTester.bind(this)}>
+            <Text>This is a tile</Text>
+          </TouchableHighlight>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: '#fff',
+    flex: 1,
+    marginTop: 40
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#6076C0'
+  }
+})
