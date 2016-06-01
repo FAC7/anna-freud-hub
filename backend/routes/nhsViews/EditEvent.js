1cf1453add84698195389ad2c18f1621a0089b41
spudtrader <jmurphy.web@gmail.com>
2016-06-01 12:48:52 +0100

    click on event loads that event in edit event view
    #107 css not loading, checkboxes not rendering

--- backend/routes/nhsViews/EditEvent.js
+++ backend/routes/nhsViews/EditEvent.js
@@ -1,13 +1,18 @@
 exports.register = (server, options, next) => {
+
+  const client = server.app.client
+  const events = server.app.events
+  const nhs = server.app.nhs
+
   server.route([ {
-    path: '/editevent/{eventId}', //how to send up eventId?
+    path: '/editevent/{eventId}',
     method: 'GET',
     config: {
       description: 'nhs wants to edit an existing event',
       auth: 'nhs',
       handler: (request, reply) => {
-        console.log(request.params.eventId, 'request params event id')
-        reply()
+        events.getEvent(client, request.params.eventId)
+          .then(data => reply.view('editEvent', { event: data }))
       }
     }
   } ])
