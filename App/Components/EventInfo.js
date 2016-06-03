3fe8addd3d0f57487e175ee2b5cf5241f1c4ae52
spudtrader <jmurphy.web@gmail.com>
2016-06-03 11:10:32 +0100

    event info abstracted into component
    #24

--- App/Components/EventInfo.js
+++ App/Components/EventInfo.js
@@ -10,7 +10,7 @@ import {
 
 import { connect } from 'react-redux'
 
-import icon from '../Assets/Icons/attending.png'
+import EventAddress from './EventAddress.js'
 
 class EventInfo extends Component {
   render () {
@@ -20,22 +20,7 @@ class EventInfo extends Component {
           style={styles.image}
           source={{ uri: this.props.activeEvent.imageUrl }}
         />
-        <View style={styles.infoContainer}>
-          <View style={styles.infoHeadersContainer}>
-            <Text style={styles.infoHeaders}>Location</Text>
-            <Text style={styles.infoHeaders}>People Attending</Text>
-          </View>
-          <View style={styles.infoContentContainer}>
-            <View style={styles.addressContainer}>
-              <Text style={styles.address}>{this.props.activeEvent.address}</Text>
-              <Text style={styles.postCode}>{this.props.activeEvent.postCode}</Text>
-            </View>
-            <View style={styles.attendingContent}>
-              <Image source={icon} style={styles.icon} />
-              <Text style={styles.attendingNumber}>{this.props.activeEvent.attending.length}</Text>
-            </View>
-          </View>
-        </View>
+        <EventAddress event={this.props.activeEvent} />
 
         <View>
           <Text>{this.props.activeEvent.creatorEmail}</Text>
@@ -60,47 +45,5 @@ const styles = StyleSheet.create({
   },
   image: {
     height: 250,
-  },
-  infoContainer: {
-    backgroundColor: '#257AC4',
-  },
-  address: {
-    color: 'white',
-    fontSize: 12
-  },
-  infoHeadersContainer: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingLeft: 10
-  },
-  infoHeaders: {
-    color: 'white',
-    fontSize: 17
-  },
-  infoContentContainer: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    padding: 10
-  },
-  attendingContent: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 5
-  },
-  icon: {
-    width: 18,
-    height: 18
-  },
-  attendingNumber: {
-    paddingLeft: 5,
-    color: '#fff',
-    fontSize: 20
-  },
-  postCode: {
-    color: '#fff',
-    fontSize: 12
-
   }
 })
