ec9b091d8efdeadd46d68f78d2725fc05d965e22
Andrew MacMurray <a.macmurray@icloud.com>
2016-06-04 21:47:23 +0100

    add content to intro screen

--- App/Components/Intro.js
+++ App/Components/Intro.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {
   Text,
   View,
+  Image,
   StyleSheet,
   TouchableOpacity
 } from 'react-native'
@@ -9,16 +10,23 @@ import {
 import { connect } from 'react-redux'
 import { newRoute } from '../Actions/actions_routing.js'
 import routes from '../Utils/routes.js'
+import introText from '../Data/IntroText.js'
+import arrow from '../Assets/Icons/arrow.png'
 
 class Intro extends Component {
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>HUB</Text>
+        <Text style={styles.subTitle}>{introText.p1}</Text>
+        <Image
+          source={arrow}
+          style={styles.arrow}
+        />
         <TouchableOpacity
           onPress={() => this.props.newRoute(routes.SIGNUP)}
         >
-          <Text>Signup</Text>
+          <Text style={styles.text}>Signup</Text>
         </TouchableOpacity>
       </View>
     )
@@ -32,10 +40,30 @@ export default connect(mapStateToProps, { newRoute })(Intro)
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#257AC4',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 60,
     flex: 1
   },
   title: {
     color: '#fff',
-    fontSize: 30
+    fontSize: 50,
+    marginTop: -100,
+    marginBottom: 50,
+    letterSpacing: 4
+  },
+  subTitle: {
+    color: '#fff',
+    fontSize: 18,
+    marginBottom: 50,
+    textAlign: 'center'
+  },
+  arrow: {
+    width: 50,
+    height: 50
+  },
+  text: {
+    marginTop: 10,
+    color: '#fff'
   }
 })
---
+++ App/Data/IntroText.js
@@ -0,0 +1,4 @@
+export default {
+  p1: 'Hub is a safe space to browse upcoming events in your local area',
+  p2: 'Enter some details, select some things that interest you and you\'re ready to go...'
+}
